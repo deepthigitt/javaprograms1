b4dfc35b7f724c70a47b002b00bf0ad8a73a3a0e
deepthisikhakolli <deepusikhakolli@gmail.com>
2024-09-25 11:45:24 -0400

    commiting all the programs

--- src/Examplefolder2/lab007.java
+++ src/Examplefolder2/lab007.java
@@ -1,4 +1,17 @@
 package Examplefolder2;
 
 public class lab007 {
+    public static void main(String[] args) {
+        int a = 50;
+        a++;
+        ++a;
+        int result = a++;
+        System.out.println(a);
+        System.out.println(result);
+        int b = 43;
+        b--;
+        --b;
+        System.out.printf("b:%d",b);
+        System.out.println((b));
+    }
 }
