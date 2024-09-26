67b336c518d99a87234fdbf7ff35353058ef8f8e
deepthisikhakolli <deepusikhakolli@gmail.com>
2024-09-26 14:16:17 -0400

    commiting 9 & 10 the programs

---
+++ src/Examplefolder3/lab008.java
@@ -0,0 +1,17 @@
+package Examplefolder3;
+
+public class lab008 {
+    public static void main(String[] args) {
+        int a = 10;
+        long b = 45;
+        String ab = "AAA";
+        String cd = "BBBB";
+        String aa = new String("deepthi");
+        System.out.println(ab+a+cd+b);;
+        System.out.println(ab+cd+(a+b));
+        System.out.println("your name:"+ab);
+        System.out.println("your name:"+cd);
+        System.out.println("Your name is:"+ab+cd);
+        System.out.println(aa);
+    }
+}
---
+++ src/Examplefolder3/lab009.java
@@ -0,0 +1,16 @@
+package Examplefolder3;
+
+public class lab009 {
+    public static void main(String[] args) {
+        String s1 = new String("Taruna");
+        String s2 = new String("Keerthi");
+        System.out.println(s1 + s2);
+        String s3 = "Janani";
+        String s4 = new String("SPURTHI");
+        String s5 = s4.toLowerCase();
+        System.out.println(s3+s4);
+        System.out.println(s5);
+        String s6 = s1.concat(s2);
+        System.out.println(s6);
+    }
+}
